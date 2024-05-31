9b713601d0f86bcdf8dfb864a9dec14cba62554a
Kevin <ko19af@brocku.ca>
2024-05-31 08:55:00 -0400

    Updated with fixes and new methods for generating topology

--- Topology.h
+++ Topology.h
@@ -8,17 +8,21 @@ using namespace std;
 class Topology {
     public:
         explicit Topology(int x = 10, int y = 10, int starts = 1, int ends = 1, int numNodes = 30);
+        int ShortestPath(int position, vector<double> &sPath);
+        void setConnections(vector<int> c);
 
     private:
         void PrintLayout();
-        int ChooseStart(int y, int numStarts);
-        int ChooseEnd(int y, int numEnds);
+        int ChooseStart(int x, int numStarts);
+        int ChooseEnd(int y, int x, int numEnds);
         void ChooseNodeLocations(int x, int y, int numNodes);
-        int ShortestPath(int position, vector<double> &sPath, vector<vector<bool>> connections);
         void findNode(int &x, int &y, int node);
+        
+        static int numNodes;// variable representing number of fog layer nodes present in the network
 
         /**
          * The layout of the network that the program will attempt to find the optimal configuration for
          */
         vector<vector<bool>> network;
+        vector<vector<int>> connections;
 };
